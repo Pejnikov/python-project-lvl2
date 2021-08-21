fd3ce34bfdd035101590824e5ddad76cc5a0dce6
Ivan Pejnikov <ivanpejnikov@Ivans-MacBook-Pro.local>
2021-08-21 23:18:40 +0300

    code readability was improved

--- gendiff/diff_engine.py
+++ gendiff/diff_engine.py
@@ -5,12 +5,15 @@ from gendiff.result_maker import get_string_result
 def generate_diff(filepath1, filepath2):
     file1 = get_file(filepath1)
     file2 = get_file(filepath2)
+    not_in_file1 = set(file2) - set(file1)
+    not_in_file2 = set(file1) - set(file2)
+    intersection = set(file1) & set(file2)
     result = []
-    for item in set(file2) - set(file1):
+    for item in not_in_file1:
         result.append(('+', item, file2[item]))
-    for item in set(file1) - set(file2):
+    for item in not_in_file2:
         result.append(('-', item, file1[item]))
-    for item in set(file1) & set(file2):
+    for item in intersection:
         if file1[item] == file2[item]:
             result.append((' ', item, file1[item]))
             continue
