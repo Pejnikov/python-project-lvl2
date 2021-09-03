b05ef99672e651b7053971a96c44868ce25aaf19
Pejnikov <ipejnikov@gmail.com>
2021-09-03 21:32:38 +0300

    Use context manager for opening files

--- gendiff/diff_engine.py
+++ gendiff/diff_engine.py
@@ -1,11 +1,11 @@
-from gendiff.parser import get_file
+from gendiff.parser import parse_data
 from gendiff.formatters.stylish import get_stylish
 from gendiff.difference import get_diff
 
 
 def generate_diff(filepath1, filepath2, formater=get_stylish):
-    file1 = get_file(filepath1)
-    file2 = get_file(filepath2)
+    file1 = parse_data(filepath1)
+    file2 = parse_data(filepath2)
     result = get_diff(file1, file2)
     result_string = formater(result)
     return result_string
--- gendiff/parser.py
+++ gendiff/parser.py
@@ -12,8 +12,14 @@ def get_args():
     return parser.parse_args()
 
 
-def get_file(filepath):
-    filepath = abspath(filepath)
+def parse_data(filepath):
+    data = get_file_data(filepath)
     if filepath.endswith('.yaml') or filepath.endswith('.yml'):
-        return yaml.load(open(filepath), Loader=yaml.Loader)
-    return json.load(open(filepath))
+        return yaml.load(data, Loader=yaml.Loader)
+    return json.loads(data)
+
+
+def get_file_data(filepath):
+    with open(abspath(filepath), 'r') as file:
+        data = file.read()
+    return data
