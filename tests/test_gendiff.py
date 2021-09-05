db2bb3830ad3ad3961d9c282bf56640dbeb2d15f
Pejnikov <ipejnikov@gmail.com>
2021-09-05 22:42:30 +0300

    Add tests for JSON formatter

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -5,6 +5,7 @@ import pytest
 
 ARTIFACTS_PATH = 'tests/fixtures/test_gendiff/'
 
+
 @pytest.mark.parametrize("file1,file2,expected_output", [(
     'hx_plain1.json', 'hx_plain2.json', 'hx_plain_exp',), (
     'hx_plain1.yaml', 'hx_plain2.yml', 'hx_plain_exp',), (
@@ -35,6 +36,19 @@ def test_plain_diff(file1, file2, expected_output):
     with open(abspath(expected_output), 'r') as file:
         data = file.read()
     assert generate_diff(file1, file2, 'plain') == data
+
+
+@pytest.mark.parametrize("file1,file2,expected_output", [(
+    'deb_rec1.json', 'deb_rec2.json', 'deb_rec_fjson_exp',
+    )
+])
+def test_json_diff(file1, file2, expected_output):
+    file1 = make_artifacts_path(file1)
+    file2 = make_artifacts_path(file2)
+    expected_output = make_artifacts_path(expected_output)
+    with open(abspath(expected_output), 'r') as file:
+        data = file.read()
+    assert generate_diff(file1, file2, 'json') == data
 
 
 def make_artifacts_path(file):
