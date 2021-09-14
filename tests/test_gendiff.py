0e504977ba9488633a96b6b79bffff1646f4de36
Pejnikov <ipejnikov@gmail.com>
2021-09-14 12:37:40 +0300

    Adjusts test fixtures

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,59 +3,71 @@ from os.path import abspath
 import pytest
 
 
-ARTIFACTS_PATH = 'tests/fixtures/test_gendiff/'
-
-
-@pytest.mark.parametrize("file1,file2,expected_output", [(
-    'hx_plain1.json', 'hx_plain2.json', 'hx_plain_exp',), (
-    'hx_plain1.yaml', 'hx_plain2.yml', 'hx_plain_exp',), (
-    'deb_rec1.json', 'deb_rec2.json', 'deb_rec_exp',), (
-    'hx_rec1.json', 'hx_rec2.json', 'hx_rec_exp',), (
-    'hx_rec1.yml', 'hx_rec2.yml', 'hx_rec_exp',
-)]
+@pytest.mark.parametrize("file1,file2,expected_output,format", [(
+    'tests/fixtures/test_gendiff/plain/hx_file1.json',
+    'tests/fixtures/test_gendiff/plain/hx_file2.json',
+    'tests/fixtures/test_gendiff/plain/hx_stylish_exp',
+    'stylish',
+), (
+    'tests/fixtures/test_gendiff/plain/hx_file1.json',
+    'tests/fixtures/test_gendiff/plain/hx_file2.json',
+    'tests/fixtures/test_gendiff/plain/hx_plain_exp',
+    'plain',
+), (
+    'tests/fixtures/test_gendiff/plain/hx_file1.yaml',
+    'tests/fixtures/test_gendiff/plain/hx_file2.yml',
+    'tests/fixtures/test_gendiff/plain/hx_stylish_exp',
+    'stylish',
+), (
+    'tests/fixtures/test_gendiff/plain/hx_file1.yaml',
+    'tests/fixtures/test_gendiff/plain/hx_file2.yml',
+    'tests/fixtures/test_gendiff/plain/hx_plain_exp',
+    'plain',
+), (
+    'tests/fixtures/test_gendiff/recursive/hx_file1.json',
+    'tests/fixtures/test_gendiff/recursive/hx_file2.json',
+    'tests/fixtures/test_gendiff/recursive/hx_stylish_exp',
+    'stylish',
+), (
+    'tests/fixtures/test_gendiff/recursive/hx_file1.json',
+    'tests/fixtures/test_gendiff/recursive/hx_file2.json',
+    'tests/fixtures/test_gendiff/recursive/hx_plain_exp',
+    'plain',
+), (
+    'tests/fixtures/test_gendiff/recursive/hx_file1.yml',
+    'tests/fixtures/test_gendiff/recursive/hx_file2.yml',
+    'tests/fixtures/test_gendiff/recursive/hx_stylish_exp',
+    'stylish',
+), (
+    'tests/fixtures/test_gendiff/recursive/hx_file1.yml',
+    'tests/fixtures/test_gendiff/recursive/hx_file2.yml',
+    'tests/fixtures/test_gendiff/recursive/hx_plain_exp',
+    'plain',
+), (
+    'tests/fixtures/test_gendiff/recursive/deb_file1.json',
+    'tests/fixtures/test_gendiff/recursive/deb_file2.json',
+    'tests/fixtures/test_gendiff/recursive/deb_stylish_exp',
+    'stylish',
+), (
+    'tests/fixtures/test_gendiff/recursive/deb_file1.json',
+    'tests/fixtures/test_gendiff/recursive/deb_file2.json',
+    'tests/fixtures/test_gendiff/recursive/deb_plain_exp',
+    'plain',
+), (
+    'tests/fixtures/test_gendiff/recursive/deb_file1.json',
+    'tests/fixtures/test_gendiff/recursive/deb_file2.json',
+    'tests/fixtures/test_gendiff/recursive/deb_json_exp',
+    'json',
+),
+]
 )
-def test_stylish_diff(file1, file2, expected_output):
-    file1 = make_artifacts_path(file1)
-    file2 = make_artifacts_path(file2)
-    expected_output = make_artifacts_path(expected_output)
+def test_diff(file1, file2, expected_output, format):
     with open(abspath(expected_output), 'r') as file:
         data = file.read()
-    assert generate_diff(file1, file2, 'stylish') == data
+    assert generate_diff(file1, file2, format) == data
 
 
-@pytest.mark.parametrize("file1,file2,expected_output", [(
-    'hx_plain1.json', 'hx_plain2.json', 'hx_plain_fplain_exp',), (
-    'deb_rec1.json', 'deb_rec2.json', 'deb_rec_fplain_exp',), (
-    'hx_rec1.json', 'hx_rec2.json', 'hx_rec_fplain_exp',
-)]
-)
-def test_plain_diff(file1, file2, expected_output):
-    file1 = make_artifacts_path(file1)
-    file2 = make_artifacts_path(file2)
-    expected_output = make_artifacts_path(expected_output)
-    with open(abspath(expected_output), 'r') as file:
-        data = file.read()
-    assert generate_diff(file1, file2, 'plain') == data
-
-
-@pytest.mark.parametrize("file1,file2,expected_output", [(
-    'deb_rec1.json', 'deb_rec2.json', 'deb_rec_fjson_exp',
-)]
-)
-def test_json_diff(file1, file2, expected_output):
-    file1 = make_artifacts_path(file1)
-    file2 = make_artifacts_path(file2)
-    expected_output = make_artifacts_path(expected_output)
-    with open(abspath(expected_output), 'r') as file:
-        data = file.read()
-    assert generate_diff(file1, file2, 'json') == data
-
-
-def make_artifacts_path(file):
-    return ARTIFACTS_PATH + file
-
-
-def test_invalid_input():
-    fake_data = make_artifacts_path('fake_data.jpeg')
+def test_wrong_extension():
+    fake_data = 'fake_data.jpeg'
     with pytest.raises(ValueError):
         generate_diff(fake_data, fake_data)
