ffdc7c492f8173c7d15bac9437c72dafa380e323
Pejnikov <ipejnikov@gmail.com>
2021-09-14 23:21:24 +0300

    Change the logic of determination for children diffs

--- gendiff/difference.py
+++ gendiff/difference.py
@@ -20,14 +20,14 @@ def get_diff(file1, file2):
             diffs.append(make_unmodified(key, file1[key]))
         elif isinstance(file1[key], dict) and isinstance(file2[key], dict):
             nested_diff = get_diff(file1[key], file2[key])
-            diffs.append(make_unmodified(key, nested_diff, nested_flag=True))
+            diffs.append(make_unmodified(key, nested_diff))
         else:
             diffs.append(make_updated(key, file1[key], file2[key]))
     return diffs
 
 
-def make_diff(flag, key, value, changed_value=None, nested=False):
-    return (flag, key, value, changed_value, nested)
+def make_diff(flag, key, value, changed_value=None):
+    return (flag, key, value, changed_value)
 
 
 def make_added(key, value):
@@ -40,8 +40,8 @@ def make_removed(key, value):
     return diff
 
 
-def make_unmodified(key, value, nested_flag=False):
-    diff = make_diff(UNMODIFIED, key, value, nested=nested_flag)
+def make_unmodified(key, value):
+    diff = make_diff(UNMODIFIED, key, value)
     return diff
 
 
@@ -67,4 +67,8 @@ def get_new_value(diff):
 
 
 def has_children(diff):
-    return diff[4]
+    if diff[0] is UNMODIFIED:
+        if isinstance(diff[2], list):
+            if isinstance(diff[2][0], tuple):
+                return True
+    return False
