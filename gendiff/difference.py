b03955fd0d90c7cf8aa1379b3a095a813b9b9484
Pejnikov <ipejnikov@gmail.com>
2021-09-22 13:52:09 +0300

    Simplify Diff making interface

--- gendiff/difference.py
+++ gendiff/difference.py
@@ -15,47 +15,22 @@ def get_diff(dict1: dict, dict2: dict) -> List[Diff]:
     common_keys = set(dict1) & set(dict2)
     diffs = []
     for key in added_keys:
-        diffs.append(make_added(key, dict2[key]))
+        diffs.append(make_diff(ADDED, key, dict2[key]))
     for key in removed_keys:
-        diffs.append(make_removed(key, dict1[key]))
+        diffs.append(make_diff(REMOVED, key, dict1[key]))
     for key in common_keys:
         if dict1[key] == dict2[key]:
-            diffs.append(make_unmodified(key, dict1[key]))
+            diffs.append(make_diff(UNMODIFIED, key, dict1[key]))
         elif isinstance(dict1[key], dict) and isinstance(dict2[key], dict):
             nested_diff = get_diff(dict1[key], dict2[key])
-            diffs.append(make_nested(key, nested_diff))
+            diffs.append(make_diff(NESTED, key, nested_diff))
         else:
-            diffs.append(make_updated(key, dict1[key], dict2[key]))
+            diffs.append(make_diff(UPDATED, key, dict1[key], dict2[key]))
     return diffs
 
 
 def make_diff(flag: str, key: str, value: Any, changed_value=None) -> Diff:
     return (flag, key, value, changed_value)
-
-
-def make_added(key: str, value: Any) -> Diff:
-    diff = make_diff(ADDED, key, value)
-    return diff
-
-
-def make_removed(key: str, value: Any) -> Diff:
-    diff = make_diff(REMOVED, key, value)
-    return diff
-
-
-def make_unmodified(key: str, value: Any) -> Diff:
-    diff = make_diff(UNMODIFIED, key, value)
-    return diff
-
-
-def make_updated(key: str, initial_value: Any, new_value: Any) -> Diff:
-    diff = make_diff(UPDATED, key, initial_value, new_value)
-    return diff
-
-
-def make_nested(key: str, value: Any) -> Diff:
-    diff = make_diff(NESTED, key, value)
-    return diff
 
 
 def get_type(diff: Tuple) -> str:
